4b9d2c294cdb266ecaaed8af0dfdaa90153c6e44
pajowu <github@ca.pajowu.de>
2016-06-29 02:09:13 +0300

    Added blink function, fixes #1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
+'use strict';
 
 function newStyle(e) {
-	style_name = e.value;
+	var style_name = e.value;
 	var style_element  = document.getElementById('logo_style');
 	style_element.href = 'css/' + style_name +'.css';
-
 }
+
+function blink(id) {
+	var eye = document.getElementById(id);
+	var blink_class = "blink";
+	if (! eye.classList.contains(blink_class))
+		eye.classList.add(blink_class);
+	var new_eye = eye.cloneNode(true);
+
+	eye.parentNode.replaceChild(new_eye, eye);
+
+	var wait_amount = 5000*Math.random();
+
+	if (wait_amount < 1000)
+		wait_amount += 1000;
+
+	console.log(wait_amount);
+
+	setTimeout(blink, wait_amount, id);
+}
+
+blink("left_eye");
+blink("right_eye");
